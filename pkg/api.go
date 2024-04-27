683b953d1287cc3130e19144e0ffba66a201422c
moali <mohammedali01071@gmail.com>
2024-04-27 18:00:19 +0300

    API error handling

--- pkg/api.go
+++ pkg/api.go
@@ -2,6 +2,7 @@ package groupietracker
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -18,8 +19,9 @@ func FetchArtists(search string) (fetchedArtists []Artist, statusCode int) {
 	defer resp.Body.Close()
 
 	// Check the response status code
+	// this returns for api errors
 	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("API request failed with status code: %d", resp.StatusCode)
+		fmt.Printf("API request failed with status code: %d\n", resp.StatusCode)
 		return artists, resp.StatusCode
 	}
 
@@ -44,7 +46,7 @@ func FetchArtistByID(id string) (fetchedArtist Artist, statusCode int) {
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("API request failed with status code: %d", resp.StatusCode)
+		fmt.Printf("API request failed with status code: %d\n", resp.StatusCode)
 		return artist, resp.StatusCode
 	}
 
@@ -65,7 +67,7 @@ func FetchArtistByID(id string) (fetchedArtist Artist, statusCode int) {
 	return artist, resp.StatusCode
 }
 
-func FetchRelationsByID(id string) RelationsPage {
+func FetchRelationsByID(id string) (relation RelationsPage, statusCode int) {
 	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/relation/" + id)
 	if err != nil {
 		log.Fatal(err)
@@ -73,120 +75,14 @@ func FetchRelationsByID(id string) RelationsPage {
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("API request failed with status code: %d", resp.StatusCode)
+		fmt.Printf("API request failed with status code: %d\n", resp.StatusCode)
+		return relation, resp.StatusCode
 	}
 
-	var relation RelationsPage
 	err = json.NewDecoder(resp.Body).Decode(&relation)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return relation
+	return relation, resp.StatusCode
 }
-
-// Might use later
-/*
-func FetchArtistByID(id string) Artist {
-	// Fetch from []Artists
-	artists := FetchArtists()
-	idInt, err := strconv.Atoi(id)
-	if err != nil {
-		log.Fatal(err)
-	}
-	artistByID := artists[idInt-1]
-	return artistByID
-}
-
-func FetchLocations() []LocationsPage {
-	// artists := FetchArtists()
-	var locations []LocationsPage = make([]LocationsPage, len(artists))
-	for ind, artist := range artists {
-		// get locations from link
-		loc, err := http.Get(artist.LocationsURL)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer loc.Body.Close()
-		// check response status code
-		if loc.StatusCode != http.StatusOK {
-			log.Fatalf("API locations request failed with status code: %d", loc.StatusCode)
-		}
-		// decode JSON locations
-		err = json.NewDecoder(loc.Body).Decode(&locations[ind])
-		if err != nil {
-			log.Fatal(err)
-		}
-
-	}
-	return locations
-}
-
-func FetchLocationsByArtistID(id string) LocationsPage {
-	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/locations/" + id)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("API request failed with status code: %d", resp.StatusCode)
-	}
-
-	var location LocationsPage
-	err = json.NewDecoder(resp.Body).Decode(&location)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return location
-}
-
-func FetchDates() []DatesPage {
-	// artists := FetchArtists()
-	var dates []DatesPage = make([]DatesPage, len(artists))
-	// get dates from link
-	for ind, artist := range artists {
-		datesJSON, err := http.Get(artist.DatesURL)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer datesJSON.Body.Close()
-		// check response status code
-		if datesJSON.StatusCode != http.StatusOK {
-			log.Fatalf("API dates request failed with status code: %d", datesJSON.StatusCode)
-		}
-		// decode JSON dates
-		err = json.NewDecoder(datesJSON.Body).Decode(&dates[ind])
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	return dates
-}
-
-func FetchRelations() []RelationsPage {
-	// artists := FetchArtists()
-	var relations []RelationsPage = make([]RelationsPage, len(artists))
-	for ind, artist := range artists {
-		// get relations from URL
-		rel, err := http.Get(artist.RelationsURL)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer rel.Body.Close()
-		// check response status code
-		if rel.StatusCode != http.StatusOK {
-			log.Fatalf("API relations request failed with status: %d", rel.StatusCode)
-		}
-		// decode json relations
-		err = json.NewDecoder(rel.Body).Decode(&relations[ind])
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	return relations
-}
-*/
